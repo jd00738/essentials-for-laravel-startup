2c61401e27ad7fd3766af1280c72acd0b0dd7ce4
Muhammad Junaid Khan <junaid.khan@codenterprise.com>
2023-03-21 17:04:11 +0500

    Models And Tinker

--- testApp/database/migrations/2023_03_21_115531_create_mine_table.php
+++ testApp/database/migrations/2023_03_21_115531_create_mine_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMyTable extends Migration
+class CreateMineTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateMyTable extends Migration
      */
     public function up()
     {
-        Schema::create('my', function (Blueprint $table) {
+        Schema::create('mies', function (Blueprint $table) {
             $table->id();
             $table->string("title");
             $table->string("description");
@@ -28,6 +28,6 @@ class CreateMyTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('my');
+        Schema::dropIfExists('mine');
     }
 }
